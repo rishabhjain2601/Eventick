9bc339fd52f70cd60750b3f17d03d87c364dffd6
shashwat-j <shashwatjain_ec21a18_23@dtu.ac.in>
2023-09-05 11:50:09 +0530

    eventPage card

---
+++ src/components/EventsPageCard.js
@@ -0,0 +1,30 @@
+import { useNavigate } from "react-router-dom"
+
+const EventsPageCard = ({event, isSelected=false}) => {
+
+    const navigate = useNavigate()
+
+    return(
+        <div onClick={()=>{navigate('/event', {state:{event: event}})}} className={`cursor-pointer w-[45vw] md:w-[250px] relative border rounded-xl flex flex-col gap-2 justify-left border-[#FFFFFF26] hover:border-red-700 `}>
+            <div className="h-[180px] md:h-[210px] relative w-full">
+                <img src={event?.event_logo} className="object-cover h-[180px] md:h-[210px] rounded-none rounded-t-xl"/>
+            </div>
+
+            <div className="px-3 pb-3 text-ellipsis overflow-hidden">
+                <div className="leading-tight text-sm pb-1">
+                    <h3 className="text-white drop-shadow-lg shadow-black">{event?.event_name?.toUpperCase()}</h3>
+                    <p className="text-white drop-shadow-lg shadow-black">{event?.organizer}</p>
+                </div>
+                <div className="leading-tight text-sm text-lightestGray">
+                    <p>{event?.start}</p>
+                    <p>{event?.venue}</p>
+                    {/* <p>{event?.genre}</p> */}
+                </div>
+                <p className="text-white text-sm pt-1">{event?.fee} | {event?.mode}</p>
+            </div>
+
+        </div>
+    )
+}
+
+export default EventsPageCard
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import SocietyCard from '../components/SocietyCard'
 import Navbar from '../components/Navbar'
 import events from '../data/events.json'
-import EventCard from '../components/EventCard'
+import EventsPageCard from '../components/EventsPageCard'
 
 const Events = () => {
 
@@ -15,10 +15,10 @@ const Events = () => {
 
         <h1 className='text-white text-center font-bold text-5xl font-roboto tracking-wide'>Events</h1>
 
-        <div className='grid gap-9 grid-cols-2 md:grid-cols-4 my-8 mx-3 lg:mx-0'>
+        <div className='grid gap-2 md:gap-9 grid-cols-2 md:grid-cols-3 my-8 mx-3 lg:mx-0'>
             {events?.map((event, index)=>(
                 <div className='mt-2' key={index}>
-                    <EventCard event={event}/>
+                    <EventsPageCard event={event}/>
                 </div>
             ))}
         </div>
